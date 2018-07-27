5c88c5938846976f3c03a2820dcdab8ae6c12367
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-07-27 15:52:20 +0400

    Angular 6 Material Starter -> Ile Maurice Booking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: String = 'Angular 6 Material Starter';
+  title: String = 'Ile Maurice Booking';
 }
