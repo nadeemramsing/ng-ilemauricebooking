dcffbe7176854653fdc91d0920f7f4ffe21e03ca
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-07-31 11:43:13 +0400

    Home and About components generated.

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,11 +2,30 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './pages.component';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
 const routes: Routes = [
   {
     path: '',
-    component: PagesComponent
-  }
+    component: PagesComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
@@ -16,5 +35,7 @@ const routes: Routes = [
 export class PagesRoutingModule { }
 
 export const PAGES_COMPONENTS = [
+  AboutComponent,
+  HomeComponent,
   PagesComponent
 ];
