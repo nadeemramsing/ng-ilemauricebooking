3e6fb106e335c7af1dae6f4da26771b69bbcb59c
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-07-31 09:06:56 +0400

    Navbar component generated and added using "ng g c common/components/navbar --spec=false --module=common".

--- src/app/common/components/export.ts
+++ src/app/common/components/export.ts
@@ -1,5 +1,7 @@
+import { NavbarComponent } from './navbar/navbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
 export const COMMON_COMPONENTS = [
+  NavbarComponent,
   SidenavComponent
 ]
