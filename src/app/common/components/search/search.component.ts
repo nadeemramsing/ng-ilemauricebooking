d3ae17388e3e1b2b4ca8b9c5d7dd111008a7527d
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-08-10 09:32:47 +0400

    <app-search> created in common

---
+++ src/app/common/components/search/search.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
+
+import * as _ from 'lodash';
+
+import { MapAPI } from './../../services/api/map.api.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SearchComponent implements OnInit {
+  public places$: Observable<any>;
+  public placeFormControl = new FormControl();
+
+  constructor(
+    private mapAPI: MapAPI,
+    private ref: ChangeDetectorRef
+  ) {
+    this.placeFormControl.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(v => this.setNewPlaces$(v));
+  }
+
+  ngOnInit() {
+    this.setNewPlaces$('');
+  }
+
+  setNewPlaces$(place: String = '') {
+    this.places$ = this.mapAPI
+      .getPlaces(place)
+      .pipe(map(v => _.chain(v.elements)
+        .sortBy('tags.name')
+        .sortedUniqBy(v => v.tags.name)
+        .value()
+      ));
+
+    this.ref.detectChanges();
+  }
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,33 +17,6 @@ import { MapAPI } from '../../common/services/api/map.api.service';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit {
-  public places$: Observable<any>;
-  public placeFormControl = new FormControl();
-
-  constructor(
-    private mapAPI: MapAPI,
-    private ref: ChangeDetectorRef
-  ) {
-    this.placeFormControl.valueChanges
-      .pipe(debounceTime(500))
-      .subscribe(v => this.setNewPlaces$(v));
-  }
-
-  ngOnInit() {
-    this.setNewPlaces$('');
-  }
-
-  setNewPlaces$(place: String = '') {
-    this.places$ = this.mapAPI
-      .getPlaces(place)
-      .pipe(map(v => _.chain(v.elements)
-        .sortBy('tags.name')
-        .sortedUniqBy(v => v.tags.name)
-        .value()
-      ));
-
-    this.ref.detectChanges();
-  }
-
+export class HomeComponent {
+  
 }
